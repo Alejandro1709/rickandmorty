ddbc2d0f4c37605d027086e7ff3352582abcd75a
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-08 15:35:11 -0500

    Add: App Navbar Markup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 function App() {
   return (
     <div className='App'>
-      <h1 className='bg-red-200'>Hola</h1>
+      <header className='flex flex-col gap-2 bg-[#272B33] h-40 w-full'>
+        <div className='flex flex-row gap-4 text-white p-4'>
+          <input
+            className='flex-1 bg-[#212329] p-2 rounded-md'
+            type='text'
+            placeholder='Search here...'
+          />
+          <button className='bg-[#212329] rounded-md'>Search</button>
+        </div>
+        <div className='flex bg-[#272B33] overflow-scroll'>
+          <ul className='flex flex-row'>
+            <li className='h-20 w-20 bg-gray-400'>Rick</li>
+            <li className='h-20 w-20 bg-gray-500'>Rick</li>
+            <li className='h-20 w-20 bg-gray-600'>Rick</li>
+            <li className='h-20 w-20 bg-gray-700'>Rick</li>
+          </ul>
+        </div>
+      </header>
+      <div className=''>BOTTOM</div>
     </div>
   );
 }
