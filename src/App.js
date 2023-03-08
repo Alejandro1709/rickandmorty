b9e83a191fc229d08644335ab71e854827610a68
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-08 16:47:22 -0500

    Add: Render characters from API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [initialCharacters] = useState(
+    JSON.parse(localStorage.getItem('characters'))
+  );
+  const [, setCharacters] = useState([]);
+  const [filteredCharacters, setFilteredCharacters] = useState([]);
+
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((res) => res.json())
+      .then(({ results }) => {
+        localStorage.setItem('characters', JSON.stringify(results));
+        setCharacters(results);
+      });
+  }, []);
+
+  const handleFilter = (e) => {
+    setInput(e.target.value);
+
+    const filteredCharacters = initialCharacters.filter((c) =>
+      c.name.toLowerCase().includes(input.toLowerCase())
+    );
+
+    setFilteredCharacters(filteredCharacters);
+
+    console.log(filteredCharacters);
+  };
+
   return (
     <>
-      <header className='flex flex-col gap-2 bg-[#272B33] h-40 w-full'>
+      <header className='flex flex-col gap-2 bg-midnight h-40 w-full'>
         <div className='flex flex-row gap-4 text-white p-4'>
           <input
-            className='flex-1 bg-[#212329] p-2 rounded-md'
+            className='flex-1 bg-midnight p-2 rounded-md'
             type='text'
             placeholder='Search here...'
+            value={input}
+            onChange={handleFilter}
           />
-          <button className='bg-[#212329] rounded-md'>Search</button>
+          <button className='bg-midnightLight rounded-md'>Search</button>
         </div>
-        <div className='flex bg-[#272B33] overflow-scroll'>
+        <div className='flex bg-midnight overflow-scroll'>
           <ul className='flex flex-row'>
-            <li className='h-20 w-20 bg-gray-400'>Rick</li>
-            <li className='h-20 w-20 bg-gray-500'>Rick</li>
-            <li className='h-20 w-20 bg-gray-600'>Rick</li>
-            <li className='h-20 w-20 bg-gray-700'>Rick</li>
+            {filteredCharacters.map((c) => (
+              <li key={c.id} className='h-20 w-20 bg-gray-400 bg-cover'>
+                <img src={c.image} alt={c.name} />
+              </li>
+            ))}
           </ul>
         </div>
       </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        midnight: '#212329',
+        midnightLight: '#212329',
+      },
+    },
   },
   plugins: [],
 };
